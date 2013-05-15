9495deaba73fec89f18be0ec9b56211d4b53e496
diana <dianagvalerio@gmail.com>
2013-05-15 19:13:46 +0200

    Create application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Curso::Application.config.secret_token = '4b3240e2757251ce667a5b18ca30a35ec42d6f51656ab54cb4467d124b61b1616bca6111305fcbf89adcfe9bb8ab36d8f5cf4f496af6cdf4ce1ad18fc0862398'
