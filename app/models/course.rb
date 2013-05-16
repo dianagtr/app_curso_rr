847e34f1e3f6fa82b0f82c0f9e27b413f5547e0d
diana <dianagvalerio@gmail.com>
2013-05-16 12:37:37 +0200

    New tests and fixtures

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,16 @@
 class Course < ActiveRecord::Base
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :active, :start_date, :location
   validates :name, :presence => true
+  validates :active, :inclusion => [true, false]
+
+  #validates_each :start_date  do |record, attr, value|
+  #  record.errors.add attr, "can't be future if active" if value < Date.today && record.active?
+  #end
+
+  validates_each :start_date do |record, attr, value|
+    error_cond = value && value < Date.today && record.active?
+    error_msg = "La fecha no puede estar en el futuro si el curso es active"
+    record.errors.add(attr, error_msg) if error_cond
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130515180031) do
+ActiveRecord::Schema.define(:version => 20130516092643) do
 
   create_table "courses", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
+    t.boolean  "active",      :default => false, :null => false
+    t.date     "start_date"
+    t.string   "location"
   end
 
 end
